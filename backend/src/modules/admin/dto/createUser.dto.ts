88bfcba3b95918a6c05a15bb5ffef4f2e6eb9d19
Marta Sanz Florez <marta.sanzflores@kanbios.fr>
2025-01-13 17:13:21 +0100

    up front and fix validation back

--- backend/src/modules/admin/dto/createUser.dto.ts
+++ backend/src/modules/admin/dto/createUser.dto.ts
@@ -1,15 +1,20 @@
 // src/auth/dto/signup.dto.ts
 import { IsEmail, IsString, Length, IsNotEmpty, Validate } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class createUserDto {
   @IsEmail({}, { message: 'Please enter a valid email.' })
+  @IsNotEmpty({ message: 'Email is required.' })
   email: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @Length(5, 255, { message: 'Password must be at least 5 characters long.' })
   password: string;
 
-  @IsString()
-  @IsNotEmpty({ message: 'Name is required.' })
-  name: string;
+   @IsString()
+   @Transform(({ value }) => value.trim())
+   @IsNotEmpty({ message: 'Name is required.' })
+   @Length(3, 50, { message: 'Name must be between 3 and 50 characters.' })
+   name: string;
 }
--- backend/src/modules/admin/dto/updateUser.dto.ts
+++ backend/src/modules/admin/dto/updateUser.dto.ts
@@ -1,11 +1,15 @@
 // src/auth/dto/signup.dto.ts
-import { IsEmail, IsString, Length, IsNotEmpty, Validate } from 'class-validator';
+import { IsEmail, IsString, Length, IsNotEmpty } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class updateUserDto {
   @IsEmail({}, { message: 'Please enter a valid email.' })
+  @IsNotEmpty({ message: 'Email is required.' })
   email: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @IsNotEmpty({ message: 'Name is required.' })
+  @Length(3, 50, { message: 'Name must be between 3 and 50 characters.' })
   name: string;
 }
